d095ae3bedf3a99bf187803c38dd49abf0ed4edb
JMichaelYang <jaewonmichael@gmail.com>
2017-07-05 23:20:47 -0700

    File setup

--- app/src/main/java/com/aopanis/forgetmenot/controller/CameraActivity.java
+++ app/src/main/java/com/aopanis/forgetmenot/controller/CameraActivity.java
@@ -1,7 +1,9 @@
-package com.aopanis.forgetmenot;
+package com.aopanis.forgetmenot.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.aopanis.forgetmenot.R;
 
 public class CameraActivity extends AppCompatActivity {
 
