9d13320b5a0eb6c9b273c6db9bd8b74413953786
JMichaelYang <jaewonmichael@gmail.com>
2017-07-08 11:24:32 -0700

    Implemented image activity

---
+++ app/src/main/java/com/aopanis/forgetmenot/controllers/ImageActivity.java
@@ -0,0 +1,15 @@
+package com.aopanis.forgetmenot.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aopanis.forgetmenot.R;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
