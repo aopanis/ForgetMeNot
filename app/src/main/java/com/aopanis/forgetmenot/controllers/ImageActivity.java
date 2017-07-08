a65650ff364b0eee06d3b321e504aa56172eb817
JMichaelYang <jaewonmichael@gmail.com>
2017-07-08 12:52:51 -0700

    Anton stuff

--- app/src/main/java/com/aopanis/forgetmenot/controllers/ImageActivity.java
+++ app/src/main/java/com/aopanis/forgetmenot/controllers/ImageActivity.java
@@ -1,21 +1,43 @@
 package com.aopanis.forgetmenot.controllers;
 
+import android.Manifest;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresPermission;
+import android.support.media.ExifInterface;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.aopanis.forgetmenot.R;
 import com.aopanis.forgetmenot.models.GalleryImage;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.FutureTarget;
 import com.bumptech.glide.request.RequestOptions;
+import com.tzutalin.dlib.Constants;
+import com.tzutalin.dlib.FaceDet;
+import com.tzutalin.dlib.VisionDetRet;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class ImageActivity extends AppCompatActivity {
 
+    private static final String TAG = "ImageActivity";
     private GalleryImage displayedImage;
 
     @Override
@@ -30,6 +52,18 @@ public class ImageActivity extends AppCompatActivity {
         RequestBuilder<Bitmap> requestBuilder = Glide.with(this).asBitmap()
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE));
         requestBuilder.load(this.displayedImage.getUri()).into(imageView);
+
+        ImageActivityPermissionsDispatcher.DetectFacesWithCheck(this);
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    public void DetectFaces() {
+        new AsyncDetectFaces(this).execute(this.displayedImage.getUri());
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_EXTERNAL_STORAGE)
+    public void showDeniedForReadExternalStorage() {
+        Toast.makeText(this, R.string.gallery_read_external_storage_denied, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -41,5 +75,40 @@ public class ImageActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected class AsyncDetectFaces extends AsyncTask<Uri, Integer, VisionDetRet[]> {
+
+        private ImageActivity activity;
+
+        public AsyncDetectFaces(ImageActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected VisionDetRet[] doInBackground(Uri... params) {
+            Bitmap bitmap = null;
+            VisionDetRet[] rets = new VisionDetRet[0];
+            try {
+                MediaStore.Images.Media.getBitmap(getContentResolver(), params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            FaceDet detector = new FaceDet(Constants.getFaceShapeModelPath());
+            Log.i(TAG, detector.detect(bitmap).toString());
+
+            return rets;
+        }
+
+        @Override
+        public void onProgressUpdate(Integer... value) {
+
+        }
+
+        @Override
+        protected void onPostExecute(VisionDetRet[] result) {
+
+        }
     }
 }
